47e8797bb42e14436e35ba3291b8e0e0ffeb310e
Chinna P <p.srinu4b1@gmail.com>
2025-11-16 11:10:38 +0530

    Updated Tc's

--- GithubActions/obj/Debug/net8.0/GithubActions.AssemblyInfo.cs
+++ GithubActions/obj/Debug/net8.0/GithubActions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GithubActions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d12422d15abeebdd17588ec56ee4656c2216d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891968cf4848a751e801dad63c0abed3160a287d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GithubActions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GithubActions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
