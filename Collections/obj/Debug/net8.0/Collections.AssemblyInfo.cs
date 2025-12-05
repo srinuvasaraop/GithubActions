7d956720d44c3d0b325d24682b7411a75ef42767
Srinivas <p.srinu4b1@gmail.com>
2025-12-05 22:23:54 +0530

    Updated TC;s

--- Collections/obj/Debug/net8.0/Collections.AssemblyInfo.cs
+++ Collections/obj/Debug/net8.0/Collections.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Collections")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24501adc802e90bce1c2a6879992fc02a042b07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e125e955d1713c4479ffd804ccd1dbdc07229b5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Collections")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Collections")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MultiThreading/obj/Debug/net8.0/MultiThreading.AssemblyInfo.cs
+++ MultiThreading/obj/Debug/net8.0/MultiThreading.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiThreading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24501adc802e90bce1c2a6879992fc02a042b07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e125e955d1713c4479ffd804ccd1dbdc07229b5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiThreading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiThreading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
